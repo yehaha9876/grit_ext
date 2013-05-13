fa68f76cd13cb901a9f389c2c66fb1bd3ece26aa
xiezhifeng <xiezf@csdn.net>
2013-05-13 15:34:36 +0800

    ref and submodule utf8 support

---
+++ lib/grit_ext/ref.rb
@@ -0,0 +1,9 @@
+module Grit
+  class Ref
+
+    def utf8_name
+      GritExt.encode! name
+    end
+
+  end
+end
---
+++ lib/grit_ext/submodule.rb
@@ -0,0 +1,9 @@
+module Grit
+  class Submodule
+
+    def utf8_name
+      GritExt.encode! name
+    end
+
+  end
+end
