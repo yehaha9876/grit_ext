1204fbebe7453b0bb73eebdd4160e1e4c8c7e41f
xiezhifeng <xiezf@csdn.net>
2013-05-24 18:58:12 +0800

    fix GPG key tag

--- lib/grit_ext/commit.rb
+++ lib/grit_ext/commit.rb
@@ -8,5 +8,58 @@ module Grit
     def utf8_short_message
       GritExt.encode! short_message
     end
+
+    # Parse out commit information into an array of baked Commit objects
+    #   +repo+ is the Repo
+    #   +text+ is the text output from the git command (raw format)
+    #
+    # Returns Grit::Commit[] (baked)
+    #
+    # really should re-write this to be more accepting of non-standard commit messages
+    # - it broke when 'encoding' was introduced - not sure what else might show up
+    #
+    # modify to skip the mergeobj for signed tags
+    #
+    def self.list_from_string(repo, text)
+      lines = text.split("\n")
+
+      commits = []
+
+      while !lines.empty?
+      	lines.shift while lines.first !~ /^commit/ # commit start
+        id = lines.shift.split.last 
+        tree = lines.shift.split.last
+
+        parents = []
+        parents << lines.shift.split.last while lines.first =~ /^parent/ 
+
+        author_line = lines.shift
+        author_line << lines.shift if lines[0] !~ /^committer/
+        author, authored_date = self.actor(author_line)
+
+        committer_line = lines.shift
+        committer_line << lines.shift if lines[0] && lines[0] != '' && lines[0] !~ /^encoding/ && lines[0] != /^mergetag/
+        committer, committed_date = self.actor(committer_line)
+
+        # not doing anything with this yet, but it's sometimes there
+        encoding = lines.shift.split.last if lines.first =~ /^encoding/
+
+        # not doing anything with this yet, but it's sometimes there
+        mergetag = []
+        mergetag << lines.shift.split.last if lines.first =~ /^mergetag/
+        mergetag << lines.shift while lines.first =~/^ /
+
+        lines.shift
+
+        message_lines = []
+        message_lines << lines.shift[4..-1] while lines.first =~ /^ {4}/
+
+        lines.shift while lines.first && lines.first.empty?
+
+        commits << Commit.new(repo, id, parents, tree, author, authored_date, committer, committed_date, message_lines)
+      end
+
+      commits
+    end
   end
 end
--- lib/grit_ext/tag.rb
+++ lib/grit_ext/tag.rb
@@ -3,5 +3,58 @@ module Grit
     def utf8_message
       GritExt.encode! message
     end
-  end
+    
+    # fix bug for GPG key tag and tag on tag
+    # at this case, the tag object is a blog, not commit
+    # do not support this type of tag so far
+    def self.find_all(repo, options = {})
+
+      refs = repo.git.refs(options, prefix)
+      refs.split("\n").map do |ref|
+        name, id = *ref.split(' ')
+        sha = repo.git.commit_from_sha(id)
+        objtype = repo.git.file_type(sha)
+        while objtype == 'tag'
+          sha = repo.git.commit_from_sha(sha)
+          objtype = repo.git.file_type(sha)
+        end
+
+        raise "Unknown object type." if sha == ''
+        
+        if objtype == 'commit'
+          commit = Commit.create(repo, :id => sha)
+        else
+          commit = nil
+        end
+
+        new(name, commit)
+      end
+    end
+
+    # fix bug for GPG key tag
+    # at this case, the tag object is a blog, not commit
+    # do not support this type of tag so far
+    def lazy_source
+
+      unless commit
+        @message  = 'unsupport tag type'
+        @tagger   = 'unsupport tag type'
+        @tag_date = 'unsupport tag type'
+        return self
+      end
+
+      data         = commit.repo.git.cat_ref({:p => true}, name)
+      @message     = commit.short_message
+      @tagger      = commit.author
+      @tag_date    = commit.authored_date
+      return self if data.empty?
+
+      if parsed = self.class.parse_tag_data(data)
+        @message  = parsed[:message]
+        @tagger   = parsed[:tagger]
+        @tag_date = parsed[:tag_date]
+      end
+      self
+    end
+
 end
